49157cef7c63f71d98d7777e7fa8f467f2a056a3
takama <justdoittakama1029@gmail.com>
2025-08-23 10:37:11 +0900

    Topページを作成

---
+++ frontend/src/features/Top/Top.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from"./style.module.css";
+import logo from "./logo.svg";
+
+function Top() {
+  const [nickname, setNickname]=useState("");
+
+  console.log("logo:", logo);
+
+  return (
+    <div className={styles.Back}> {/*最奥の背景*/}
+      <div className={styles.card}> {/*白いカード */}
+        <img src={logo.src} style={{width:"340px",height:"auto",marginTop:"70px"}}/>
+
+        <p className={styles.subtitle}>力を合わせてパズルを完成させよう!</p>
+        
+        <div className={styles.inputGroup}>
+          <label>名前 : </label>
+          <input
+            type="text"
+            id="nickname"
+            placeholder="ニックネームを入力"
+            value={nickname}
+            onChange={(e)=>setNickname(e.target.value)}
+          />
+        </div>
+        
+        <div className={styles.buttonGroup}>
+          <button className={styles.createButton} onClick={()=>console.log("チームを作る")}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="70px" viewBox="0 -960 960 960" width="70px" fill="rgb(74, 44, 0);"><path d="M0-240v-63q0-43 44-70t116-27q13 0 25 .5t23 2.5q-14 21-21 44t-7 48v65H0Zm240 0v-65q0-32 17.5-58.5T307-410q32-20 76.5-30t96.5-10q53 0 97.5 10t76.5 30q32 20 49 46.5t17 58.5v65H240Zm540 0v-65q0-26-6.5-49T754-397q11-2 22.5-2.5t23.5-.5q72 0 116 26.5t44 70.5v63H780Zm-455-80h311q-10-20-55.5-35T480-370q-55 0-100.5 15T325-320ZM160-440q-33 0-56.5-23.5T80-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T160-440Zm640 0q-33 0-56.5-23.5T720-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T800-440Zm-320-40q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-600q0 50-34.5 85T480-480Zm0-80q17 0 28.5-11.5T520-600q0-17-11.5-28.5T480-640q-17 0-28.5 11.5T440-600q0 17 11.5 28.5T480-560Zm1 240Zm-1-280Z"/></svg>
+            <span>チームを作る</span>
+          </button>
+          <button className={styles.joinButton} onClick={()=>console.log("チームに参加")}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="70px" viewBox="0 -960 960 960" width="70px" fill="rgb(74, 44, 0);"><path d="M360-80v-529q-91-24-145.5-100.5T160-880h80q0 83 53.5 141.5T430-680h100q30 0 56 11t47 32l181 181-56 56-158-158v478h-80v-240h-80v240h-80Zm120-640q-33 0-56.5-23.5T400-800q0-33 23.5-56.5T480-880q33 0 56.5 23.5T560-800q0 33-23.5 56.5T480-720Z"/></svg>
+            <span>チームに参加</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Top
