bde2210732632af4db6b5b53bec06fa4b8de7690
takama <justdoittakama1029@gmail.com>
2025-08-26 19:50:20 +0900

    画面更新

--- frontend/src/features/TeamNumberInput/TeamNumberInput.tsx
+++ frontend/src/features/TeamNumberInput/TeamNumberInput.tsx
@@ -7,27 +7,23 @@ export const TeamNumberInput = () => {
 
   return (
     <div
-      className="w-full bg-white"
+      className="flex flex-col justify-center items-center min-h-screen bg-white px-5"
       style={{ fontFamily: "'Noto Sans JP', sans-serif" }}
     >
-      <button
-        className="absolute top-[20px] left-[10px] w-[45px] h-[45px] bg-[#2EAFB9] rounded-full flex items-center justify-center text-white text-[20px] font-bold shadow-[0_2px_4px_gray] active:shadow-[0-gray] active:translate-y-[5px]"
-      >
+      <button className="absolute top-3 left-3 w-11 h-11 bg-[#2EAFB9] rounded-full justify-center text-white font-bold shadow-[0_2px_4px_gray] active:shadow-none active:translate-y-1">
         く
       </button>
 
-      <div className="bg-white border-2 border-[#00bcd4] rounded-[12px] p-6 text-center shadow-md mt-50 mx-5">
+      <div className="w-full bg-white border-2 border-[#00bcd4] rounded-xl p-6 text-center shadow-xl">
         <h1 className="text-[22px] font-bold mt-4 mb-2">チーム番号を入力</h1>
-        <div className="w-[80px] h-[6px] bg-gradient-to-r from-[#007f9e] to-[#00c8ff] mx-auto mb-6 mt-[20px] mb-[60px]"/> 
-        <p className="text-[14px] mb-1">
-          チームを作成した人の画面に映っている番号を入力してください。
-        </p>
-        <p className="text-[14px] mb-6">(例: XDG3048)</p>
+        <div className="w-20 h-1 bg-gradient-to-r from-[#007f9e] to-[#00c8ff] mx-auto mb-6 mt-5 mb-15"/> 
+        <p className="mb-1">チームを作成した人の画面に映っている番号を入力してください。</p>
+        <p className="mb-6">(例: XDG3048)</p>
 
-        <div className="flex items-center gap-x-2 max-w-[300px] mx-auto my-[30px]">
+        <div className="flex items-center gap-x-2 max-w-75 mx-auto my-7">
           <label
             htmlFor="joinNumber"
-            className="font-bold text-[18px] min-w-[100px] text-left"
+            className="font-bold min-w-25 text-left"
           >
             チーム番号:
           </label>
@@ -37,32 +33,28 @@ export const TeamNumberInput = () => {
             placeholder="チーム番号を入力"
             value={joinNumber}
             onChange={(e) => setJoinNumber(e.target.value)}
-            className="w-full p-[7px] text-[15px] rounded-[8px] border-2 border-[#007f9e]"
+            className="w-full p-2 rounded-lg border-2 border-[#007f9e]"
           />
         </div>
 
         <button
-          className="mt-4 px-8 py-3 bg-[#ffba39] text-[#333] rounded-[14px] shadow-[0_4px_8px_#ffba39] active:shadow-[0_2px_4px_#ffba39] active:translate-y-[5px] border-2 border-[#8a5a00]"
+          className="mt-4 px-8 py-3 bg-[#ffba39] rounded-xl shadow-[0_4px_8px_#ffba39] active:shadow-[0_2px_4px_#ffba39] active:translate-y-1 border-2 border-[#8a5a00]"
         >
           チームに参加！
         </button>
       </div>
 
-     {/* 波形グラフィック */}
-      <div className="mt-40">
-        <svg viewBox="0 0 393 73" className="w-full h-[73px]" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <linearGradient id="paint0_linear_12_62" x1="196.5" y1="0" x2="196.5" y2="73" gradientUnits="userSpaceOnUse">
-              <stop stopColor="#2EAFB9" />
-              <stop offset="1" stopColor="#27A2AA" />
-            </linearGradient>
-          </defs>
-          <path
-            d="M19.65 7.665C13.1 12.775 6.55 15.33 0 15.33L0 73H393V15.33C386.45 15.33 379.9 12.775 373.35 7.665C360.25 -2.555 347.15 -2.555 334.05 7.665C327.5 12.775 320.95 15.33 314.4 15.33C307.85 15.33 301.3 12.775 294.75 7.665C281.65 -2.555 268.55 -2.555 255.45 7.665C248.9 12.775 242.35 15.33 235.8 15.33C229.25 15.33 222.7 12.775 216.15 7.665C203.05 -2.555 189.95 -2.555 176.85 7.665C170.3 12.775 163.75 15.33 157.2 15.33C150.65 15.33 144.1 12.775 137.55 7.665C124.45 -2.555 111.35 -2.555 98.25 7.665C91.7 12.775 85.15 15.33 78.6 15.33C72.05 15.33 65.5 12.775 58.95 7.665C45.85 -2.555 32.75 -2.555 19.65 7.665Z"
-            fill="url(#paint0_linear_12_62)"
-          />
-        </svg>
-      </div>
+      <svg viewBox="0 0 393 73" preserveAspectRatio="none" className="fixed bottom-0 w-screen h-17" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <linearGradient id="paint0_linear_12_62" x1="196.5" y1="0" x2="196.5" y2="73" gradientUnits="userSpaceOnUse">
+            <stop stopColor="#2EAFB9" />              <stop offset="1" stopColor="#27A2AA" />
+          </linearGradient>
+        </defs>
+        <path
+          d="M19.65 7.665C13.1 12.775 6.55 15.33 0 15.33L0 73H393V15.33C386.45 15.33 379.9 12.775 373.35 7.665C360.25 -2.555 347.15 -2.555 334.05 7.665C327.5 12.775 320.95 15.33 314.4 15.33C307.85 15.33 301.3 12.775 294.75 7.665C281.65 -2.555 268.55 -2.555 255.45 7.665C248.9 12.775 242.35 15.33 235.8 15.33C229.25 15.33 222.7 12.775 216.15 7.665C203.05 -2.555 189.95 -2.555 176.85 7.665C170.3 12.775 163.75 15.33 157.2 15.33C150.65 15.33 144.1 12.775 137.55 7.665C124.45 -2.555 111.35 -2.555 98.25 7.665C91.7 12.775 85.15 15.33 78.6 15.33C72.05 15.33 65.5 12.775 58.95 7.665C45.85 -2.555 32.75 -2.555 19.65 7.665Z"
+          fill="url(#paint0_linear_12_62)" 
+         />
+      </svg>
     </div>
   );
 };
